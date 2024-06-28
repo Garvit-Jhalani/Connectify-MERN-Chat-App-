90e4af54c2c9159118e59d8f0a8b37ad4fba2133
Garvit-Jhalani <121152385+Garvit-Jhalani@users.noreply.github.com>
2024-06-28 14:09:27 +0530

    Third

--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -35,7 +35,7 @@ const Homepage = () => {
         borderWidth="1px"
       >
         <Text ml={40} pl={1} fontSize="4xl" fontFamily="Work sans">
-          Talk-A-Tive
+          Connectify
         </Text>
       </Box>
       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
